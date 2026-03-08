e7795faa1665954c6782972dd56d570e9664ebb8
Fadhil Syahidan <fadilsyahidan14@gmail.com>
2026-03-08 13:00:41 +0700

    commit 5 bil prima

--- Pertemuan 3/bilPrima.dart
+++ Pertemuan 3/bilPrima.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   print("Program Cek Bilangan Prima");
 
-  stdout.write("Masukkan sebuah angka: ");
+  stdout.write("Masukkan sebuah angka bulat: ");
   int angka = int.parse(stdin.readLineSync()!);
 
   bool prima = true;
