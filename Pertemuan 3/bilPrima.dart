88fb685e2a0009704a358b2cd8547b44e1eeee4d
Fadhil Syahidan <fadilsyahidan14@gmail.com>
2026-03-08 13:14:40 +0700

    commit 4: menambahkan perulangan untuk mengecek pembagi

--- Pertemuan 3/bilPrima.dart
+++ Pertemuan 3/bilPrima.dart
@@ -10,5 +10,12 @@ void main() {
 
   if (angka <= 1) {
     prima = false;
+  } else {
+    for (int i = 2; i <= angka ~/ 2; i++) {
+      if (angka % i == 0) {
+        prima = false;
+        break;
+      }
+    }
   }
 }
